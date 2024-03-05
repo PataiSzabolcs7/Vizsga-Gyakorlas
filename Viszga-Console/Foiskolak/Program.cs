e0dec3d0ff29f91c66236231115d6a6c058211f7
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2024-03-05 10:48:53 +0100

    Add files via upload

---
+++ Viszga-Console/Foiskolak/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Foiskolak;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Foiskolak
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<Foiskola> Foiskolak = new List<Foiskola>();
+            Foiskolak = await foiskolakAdatok();
+            foreach(Foiskola foiskolak in Foiskolak)
+            {
+                Console.WriteLine($"{foiskolak.Name} - {foiskolak.Country}");
+            }
+            Console.ReadKey();
+           
+        }
+
+        private static async Task<List<Foiskola>> foiskolakAdatok()
+        {
+            List<Foiskola> foiskolak = new List<Foiskola>();
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://universities.hipolabs.com/search?country=hungary");
+            if(response.IsSuccessStatusCode)
+            {
+                
+                string jsonString = await response.Content.ReadAsStringAsync();
+                foiskolak = Foiskola.FromJson(jsonString).ToList();
+                
+            }
+            return foiskolak;
+        }
+    }
+}
---
+++ Viszga-Console/Penzvalto/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Valutavalto;
+
+namespace Penzvalto
+{
+    internal class Program
+    {
+        static Valuta valuta = null;
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Mennyit szeretne átváltani?");
+            double szam = int.Parse(Console.ReadLine());
+            await valutaValto();
+            var euro = szam / valuta.Rates["HUF"];
+            var dollar = (szam / valuta.Rates["HUF"])*valuta.Rates["USD"];
+            Console.WriteLine($"{szam} HUF = {euro} EUR");
+            Console.WriteLine($"{szam} HUF = {dollar} USD");
+
+
+            Console.ReadKey();
+
+        }
+        private static async Task valutaValto()
+        {
+            string url = $"https://infojegyzet.hu/webszerkesztes/php/valuta/api/v1/arfolyam/";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            string jsonString = await response.Content.ReadAsStringAsync();
+            valuta = Valuta.FromJson(jsonString);
+
+        }
+    }
+}
